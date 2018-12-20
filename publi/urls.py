411548ac2601a3e3090d337117aa152808fe5af2
Emmanuel Roubin <emmanuel.roubin@univ-grenoble-alpes.fr>
2018-12-20 17:10:04 +0100

    teams

--- publi/urls.py
+++ publi/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = "publi"
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
+    re_path(r'^labo/$', views.labo, name='labo'),
+    re_path(r'^rv/$', views.rv, name='rv'),
+    re_path(r'^comhet/$', views.comhet, name='comhet'),
+    re_path(r'^geomecanique/$', views.geomecanique, name='geomecanique'),
     ]
--- publi/views.py
+++ publi/views.py
@@ -6,11 +6,79 @@ def index(request):
     from time import strftime
     from time import gmtime
 
-    publis = requests.get('http://api.archives-ouvertes.fr/search/3S-R?fq=docType_s:ART&sort=producedDate_tdate desc&rows=100&fl=*').json()
+    publis = requests.get('http://api.archives-ouvertes.fr/search/3S-R?q=(docType_s:ART)&sort=producedDate_tdate desc&rows=100&fl=*').json()
 
     date = strftime("%d/%m/%Y", gmtime())
     context = {"publis": publis["response"]["docs"],
                "publis3": publis["response"]["docs"][:3],
                "publis5": publis["response"]["docs"][:5],
                "date": date}
+    return render(request, 'publi.html', context)
+
+
+def labo(request):
+    import requests
+    from time import strftime
+    from time import gmtime
+
+    structure = {"name": "Laboratoire", "id": 706}
+    publis = requests.get("http://api.archives-ouvertes.fr/search/?q=(docType_s:ART AND authStructId_i:{})&sort=producedDate_tdate desc&rows=100&fl=*".format(structure.get("id"))).json()
+
+    date = strftime("%d/%m/%Y", gmtime())
+    context = {"publis": publis["response"]["docs"],
+               "publis3": publis["response"]["docs"][:3],
+               "publis5": publis["response"]["docs"][:5],
+               "date": date,
+               "structure": structure}
+    return render(request, 'publi.html', context)
+
+
+def comhet(request):
+    import requests
+    from time import strftime
+    from time import gmtime
+
+    structure = {"name": "CoMHet", "id": 545341}
+    publis = requests.get("http://api.archives-ouvertes.fr/search/?q=(docType_s:ART AND authStructId_i:{})&sort=producedDate_tdate desc&rows=100&fl=*".format(structure.get("id"))).json()
+
+    date = strftime("%d/%m/%Y", gmtime())
+    context = {"publis": publis["response"]["docs"],
+               "publis3": publis["response"]["docs"][:3],
+               "publis5": publis["response"]["docs"][:5],
+               "date": date,
+               "structure": structure}
+    return render(request, 'publi.html', context)
+
+
+def geomecanique(request):
+    import requests
+    from time import strftime
+    from time import gmtime
+
+    structure = {"name": "Géomécanique", "id": 545340}
+    publis = requests.get("http://api.archives-ouvertes.fr/search/?q=(docType_s:ART AND authStructId_i:{})&sort=producedDate_tdate desc&rows=100&fl=*".format(structure.get("id"))).json()
+
+    date = strftime("%d/%m/%Y", gmtime())
+    context = {"publis": publis["response"]["docs"],
+               "publis3": publis["response"]["docs"][:3],
+               "publis5": publis["response"]["docs"][:5],
+               "date": date,
+               "structure": structure}
+    return render(request, 'publi.html', context)
+
+
+def rv(request):
+    import requests
+    from time import strftime
+    from time import gmtime
+
+    structure = {"name": "RV", "id": 545342}
+    publis = requests.get("http://api.archives-ouvertes.fr/search/?q=(docType_s:ART AND authStructId_i:{})&sort=producedDate_tdate desc&rows=100&fl=*".format(structure.get("id"))).json()
+
+    date = strftime("%d/%m/%Y", gmtime())
+    context = {"publis": publis["response"]["docs"],
+               "publis3": publis["response"]["docs"][:3],
+               "publis5": publis["response"]["docs"][:5],
+               "date": date,
+               "structure": structure}
     return render(request, 'publi.html', context)
