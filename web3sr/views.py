78379f5e80dc2de9914c203c91d82bdbe9e2e6cd
Emmanuel Roubin <emmanuel.roubin@univ-grenoble-alpes.fr>
2019-02-12 10:12:19 +0100

    redirect / to /publi

--- web3sr/views.py
+++ web3sr/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
 
 def index(request):
-    return render(request, 'web3sr.html')
+    # return render(request, 'web3sr.html')
+    return HttpResponseRedirect(reverse('publi:index'))
