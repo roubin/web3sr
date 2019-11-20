04a6cd58968e056fa2bbca19edebb794e4cc711e
Emmanuel Roubin <emmanuel.roubin@3sr-grenoble.fr>
2019-11-20 15:44:19 +0100

    last 100

--- publi/views.py
+++ publi/views.py
@@ -7,13 +7,12 @@ from time import gmtime
 REQ_DOC_TYPE = "(docType_s:ART OR docType_s:OUV OR docType_s:COUV)"
 BRIDGE_TYPES = {"ART": "Articles", "COMM": "Conferences", "COUV": "Books", "THESE": "These"}
 SORT_BY = "producedDate_s"  # "journalDate_s"
-N_PUBLI_MAX = 1000
+N_PUBLI_MAX = 100
 
 
 def index(request):
     structure = {"name": "Collection 3SR", "id": "3S-R"}
     req = "http://api.archives-ouvertes.fr/search/{}?q={}&sort={} desc&rows={}&fl=*".format(structure.get("id"), REQ_DOC_TYPE, SORT_BY, N_PUBLI_MAX)
-    print(req)
     publis = requests.get(req).json()
     date = strftime("%d/%m/%Y", gmtime())
     context = {"publis": publis["response"]["docs"],
@@ -28,13 +27,10 @@ def index(request):
 def labo(request):
     structure = {"name": "Laboratoire", "id": 706}
     req = "http://api.archives-ouvertes.fr/search/?q=({r} AND authStructId_i:{s})&sort={so} desc&rows={n}&fl=*".format(s=structure.get("id"), r=REQ_DOC_TYPE, n=N_PUBLI_MAX, so=SORT_BY)
-    print(req)
     publis = requests.get(req).json()
 
     req = "https://api.archives-ouvertes.fr/ref/author/?q=structureId_i:706"
-    print(req)
     authors = requests.get(req).json()
-    print(authors)
 
     date = strftime("%d/%m/%Y", gmtime())
     context = {"publis": publis["response"]["docs"],
@@ -49,7 +45,6 @@ def labo(request):
 def comhet(request):
     structure = {"name": "CoMHet", "id": 545341}
     req = "http://api.archives-ouvertes.fr/search/?q=({r} AND authStructId_i:{s})&sort={so} desc&rows={n}&fl=*".format(s=structure.get("id"), r=REQ_DOC_TYPE, n=N_PUBLI_MAX, so=SORT_BY)
-    print(req)
     publis = requests.get(req).json()
 
     date = strftime("%d/%m/%Y", gmtime())
@@ -65,7 +60,6 @@ def comhet(request):
 def geomecanique(request):
     structure = {"name": "Géomécanique", "id": 545340}
     req = "http://api.archives-ouvertes.fr/search/?q=({r} AND authStructId_i:{s})&sort={so} desc&rows={n}&fl=*".format(s=structure.get("id"), r=REQ_DOC_TYPE, n=N_PUBLI_MAX, so=SORT_BY)
-    print(req)
     publis = requests.get(req).json()
 
     date = strftime("%d/%m/%Y", gmtime())
@@ -81,7 +75,6 @@ def geomecanique(request):
 def rv(request):
     structure = {"name": "RV", "id": 545342}
     req = "http://api.archives-ouvertes.fr/search/?q=({r} AND authStructId_i:{s})&sort={so} desc&rows={n}&fl=*".format(s=structure.get("id"), r=REQ_DOC_TYPE, n=N_PUBLI_MAX, so=SORT_BY)
-    print(req)
     publis = requests.get(req).json()
 
     date = strftime("%d/%m/%Y", gmtime())
@@ -96,11 +89,9 @@ def rv(request):
 
 def idHal(request):
     idHal = request.GET.get('q', False)
-    print("idHal: {}".format(idHal))
     author = {"idHal": idHal}
     # req = "http://api.archives-ouvertes.fr/search/?q=({r} AND authIdHal_s:{s})&sort={so} desc&rows={n}&fl=*".format(s=structure.get("id"), r=REQ_DOC_TYPE, n=N_PUBLI_MAX, so=SORT_BY)
     req = "http://api.archives-ouvertes.fr/search/?q=(authIdHal_s:{s})&sort={so} desc&rows={n}&fl=*".format(s=author.get("idHal"), n=N_PUBLI_MAX, so=SORT_BY)
-    print(req)
     publis_by_type = dict()
     api_error = dict()
     publis = requests.get(req).json()
@@ -109,11 +100,9 @@ def idHal(request):
     else:
         publis = publis["response"]["docs"]
         req_author = 'https://api.archives-ouvertes.fr/ref/author/?q=idHal_s:{s}'.format(s=author.get("idHal"))
-        print(req_author)
         idhal_info = requests.get(req_author).json()
         author["docid"] = idhal_info.get("response")["docs"]
         for i, publi in enumerate(publis):
-            print(i, publi["citationFull_s"])
             type = BRIDGE_TYPES.get(publi.get("docType_s")) if BRIDGE_TYPES.get(publi.get("docType_s")) is not None else publi.get("docType_s")
             if type in publis_by_type:
                 publis_by_type[type].append(publi)
